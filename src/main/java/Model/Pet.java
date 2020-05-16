1681e7bae1ae2c24303f69b111d633f3428b59c6
Anna Cetnar <anna.cetnar@gmail.com>
2020-05-16 17:51:36 +0200

    first commit

---
+++ src/main/java/Model/Pet.java
@@ -0,0 +1,19 @@
+package Model;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pet {
+
+    private  Long id;
+    private String name;
+    private int age;
+    private String ownerName;
+    private double weight;
+    private boolean pureRace;
+}
